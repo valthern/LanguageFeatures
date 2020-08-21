6c66e9dc9e2a535e5c92398a3ea0096e820619c6
Omar I. Martínez Gómez <omar_238@hotmail.com>
2020-08-21 00:28:41 -0500

    pp 74-98

---
+++ LanguageFeatures/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using LanguageFeatures.Models;
+
+namespace LanguageFeatures.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            List<string> results = new List<string>();
+
+            foreach (Product p in Product.GetProducts())
+            {
+                string name = p?.Name;
+                decimal? price = p?.Price;
+                results.Add(string.Format("Name: {0}, Price: {1}", name, price));
+            }
+
+            return View(results);
+        }
+    }
+}
