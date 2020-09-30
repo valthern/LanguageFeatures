25c0fcdfef49eb0ab7e4815123ab064f5fdb8702
Omar I. Martínez Gómez <omar_238@hotmail.com>
2020-09-30 00:15:28 -0500

    pp 88-112

--- LanguageFeatures/Controllers/HomeController.cs
+++ LanguageFeatures/Controllers/HomeController.cs
@@ -8,24 +8,20 @@ namespace LanguageFeatures.Controllers
     {
         public ViewResult Index()
         {
-            object[] data = new object[] {275M, 29.95M, "apple", "orange", 100, 10};
+            ShoppingCart cart = new ShoppingCart{Products = Product.GetProducts()};
 
-            decimal total = 0;
+            Product[] productArray = {
+                new Product{Name = "Kayak", Price = 275M},
+                new Product{Name = "Lifejacket", Price = 48.95M}
+            };
 
-            for (int i = 0; i < data.Length; i++)
-            {
-                switch (data[i])
-                {
-                    case decimal decimalValue:
-                        total += decimalValue;
-                        break;
-                    case int intValue when intValue > 50:
-                        total += intValue;
-                        break;
-                }
-            }
+            decimal cartTotal = cart.TotalPrices();
+            decimal arrayTotal = productArray.TotalPrices();
 
-            return View("Index", new string[] {$"Total: {total:C2}"});
+            return View("Index", new string[] {
+                $"Cart Total: {cartTotal:C2}",
+                $"Array Total: {arrayTotal:C2}"
+            });
         }
     }
 }
