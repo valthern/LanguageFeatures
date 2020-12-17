3f5d822ce3f9d4e1deed123eb27369c8a0eaa8f8
Omar I. Martínez Gómez <omar_238@hotmail.com>
2020-12-17 00:17:40 -0600

    pp 100-124

---
+++ LanguageFeatures/Models/MyAsyncMethods.cs
@@ -0,0 +1,13 @@
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace LanguageFeatures.Models
+{
+    public class MyAsyncMethods
+    {
+        public static Task<long?> GetPageLength()
+        {
+            HttpClient client = 
+        }
+    }
+}
